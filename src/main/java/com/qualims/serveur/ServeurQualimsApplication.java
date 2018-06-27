c092a9575bdd6c90f152d855e7dd9c2044c2eb3c
Franck <franck.lamy@qualims.com>
2018-06-27 09:36:24 +0200

    Commit initial

---
+++ src/main/java/com/qualims/serveur/ServeurQualimsApplication.java
@@ -0,0 +1,12 @@
+package com.qualims.serveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurQualimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurQualimsApplication.class, args);
+	}
+}
